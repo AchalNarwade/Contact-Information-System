390d88f3bbc6c70b9fce396430189d09d53b1895
AchalNarwade <achalnarwade211@gmail.com>
2026-03-16 18:44:19 +0530

    Implemented Contact class with fields, constructor, and getter methods

--- src/ContactInfoSystem/Contact.java
+++ src/ContactInfoSystem/Contact.java
@@ -1,4 +1,48 @@
 package ContactInfoSystem;
 
 public class Contact {
+//PRIVATE VARIABLES
+    private String name;
+    private String phoneNumber;
+    private String email;
+    private String address;
+
+//CONSTRUCTOR
+    public Contact(String name,String phoneNumber, String email,String address ){
+        this.name = name;
+        this.phoneNumber= phoneNumber;
+        this.email = email;
+        this.address = address;
+    }
+//DISPLAY METHODS
+    public void displayContact(){
+        System.out.println("CONTACT Details: ");
+        System.out.println(
+                "\nName    : " + name +
+                "\nPhone   : " + phoneNumber +
+                "\nEmail   : " + email +
+                "\nAddress : " + address
+        );
+    }
+
+
+
+//GETTER METHODS
+    public String getName(){
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+
 }
