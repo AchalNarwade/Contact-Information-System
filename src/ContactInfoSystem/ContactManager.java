c68a16c51b44a8314b3a7966b6d17273a947bd44
AchalNarwade <achalnarwade211@gmail.com>
2026-03-16 19:27:21 +0530

    Implement ContactManager with contact list management methods

--- src/ContactInfoSystem/ContactManager.java
+++ src/ContactInfoSystem/ContactManager.java
@@ -1,4 +1,39 @@
 package ContactInfoSystem;
 
+import java.util.ArrayList;
+
 public class ContactManager {
+
+    private ArrayList<Contact> contacts = new ArrayList<>();
+
+    public void addContact(Contact contact){
+        if(findContact(contact.getName()) !=null) {
+            System.out.println("Contact with Name: " + contact.getName() + "already exists!");
+            return;
+        }
+        contacts.add(contact);
+        System.out.println("Contact Successfully Added");
+    }
+
+    public void showAllContacts(){
+        if(contacts.isEmpty()){
+            System.out.println("No Contacts Available!");
+        }else{
+            for(Contact c : contacts){
+                c.displayContact();
+            }
+        }
+    }
+
+    public Contact findContact(String name){
+        for(Contact c : contacts){
+            if(c.getName().equals(name)){
+                return c;
+            }
+        }
+        return null;
+    }
+
+
+    public int getTotalContacts(){ return contacts.size();}
 }
